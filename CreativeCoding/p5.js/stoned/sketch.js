84d3abd9ad371d81e81ea3f579bff739a193f9c6
Adison Rossiter <ross0375@algonquinlive.com>
2022-07-10 14:13:12 -0400

    a

--- CreativeCoding/p5.js/stoned/sketch.js
+++ CreativeCoding/p5.js/stoned/sketch.js
@@ -32,30 +32,25 @@ function draw() {
   //   vertex(x,y);
   // }
   // endShape();
-  drawCircleBasedOnFreq(windowWidth / 3, windowHeight / 2, windowWidth / 2, "bass");
+  drawLines(windowWidth / 5 * 0 + (windowWidth / 10), windowHeight / 2, 100, 'bass');
 
-  drawASmileyFace(windowWidth / 2, windowHeight / 2, windowWidth / 2);
 }
 
 //takes the frequencyResponse and maps it to a circle
-function drawCircleBasedOnFreq(x, y, radius, freq) {
+
+function drawACircleBasedonFreq(x, y, radius, freq) {
   let freqAn = fft.getEnergy(freq);
-  let mappedFreq = map(freqAn, 0, 255, 0, radius);
+  let h = -height +map(freqAn, 0 , 255, height, 0);
+  let r = map(freqAn, 0 , 255, 0, radius);
   stroke(0,255,0);
   fill(0,255,0);
-  ellipse(x, y, mappedFreq, mappedFreq);
+  ellipse(x, y, r, r);
 }
 
-function turnTheCanvasIntoAKaleidescope(){
-  let waveform = fft.waveform();
-  let mappedWaveform = map(waveform, -1, 1, 0, height);
-  
-  beginShape();
-  for (let i = 0; i < waveform.length; i++){
-    let x = map(i, 0, waveform.length, 0, width);
-    let y = map( waveform[i], -1, 1, 0, height);
-    rotateVertexOverTime(x, y, height, i);
-    vertex(x,y);
-  }
-  endShape();
+function drawLines(x, y, radius, freq) {
+  let freqAn = fft.getEnergy(freq);
+  let h = -height +map(freqAn, 0 , 255, height, 0);
+  let r = map(freqAn, 0 , 255, 0, radius);
+  stroke(0,255,0);
+  line(x, y, x, y + h);
 }
