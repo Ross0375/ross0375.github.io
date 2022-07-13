fd4cef7951a2c137d665a5a5938d64f296ddf245
Adison Rossiter <ross0375@algonquinlive.com>
2022-07-13 12:38:43 -0400

    class work

---
+++ MTM6404/vite-project/src/App.vue
@@ -0,0 +1,65 @@
+<!-- Template (html) -->
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="display-4 py-4">
+          Grocery List
+        </h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <ListGroup>
+          <ListGroupItem 
+            v-for="(item, index) in items"
+            :key="index"
+            :index="index"
+            :text="item.text"
+            :initial-completed="item.completed"
+            @update-item="updateItem"
+          />
+        </ListGroup>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+<!-- Logic (js) -->
+<script>
+  import ListGroupItem from './components/ListGroupItem.vue'
+  import ListGroup from './components/ListGroup.vue'
+
+  export default {
+    name: 'App',
+    components: { ListGroupItem, ListGroup },
+    data: function () {
+      return {
+        items: [
+          { text: 'Milk', completed: false},
+          { text: 'Egg', completed: false},
+          { text: 'Bees', completed: false},
+          { text: 'Rice', completed: false},
+          { text: 'Cereal', completed: false},
+          { text: 'Bones', completed: false},
+          { text: 'Goose', completed: true},
+          { text: 'Chicken', completed: false}
+        ]
+      }
+    },
+    methods: {
+      updateItem: function (index,completed){
+        this.items[index].completed = completed
+      }
+    }
+  }
+</script>
+
+
+<!-- Style -->
+
+<style>
+
+</style>
---
+++ MTM6404/vite-project/src/components/ListGroup.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul class="list-group">
+        <slot></slot>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'ListGroup'
+}
+</script>
+
+<style>
+
+</style>
